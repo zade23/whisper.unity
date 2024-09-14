4c99f47b9ee26a40a8889d17d90a64ccbd4eface
zhanganduo <zhanganduo@aobi.com>
2024-09-14 18:23:18 +0800

    Add UnityWebRequest sample files

---
+++ Assets/Samples/7 - WebSearch/KeywordSearch.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class KeywordSearch : MonoBehaviour
+{
+    InputField outputArea;
+    InputField inputKeyword;
+    Button searchButton;
+
+    void Start()
+    {
+        // 获取组件
+        outputArea = GameObject.Find("OutputArea").GetComponent<InputField>();
+        inputKeyword = GameObject.Find("InputKeyword").GetComponent<InputField>();
+        searchButton = GameObject.Find("SearchButton").GetComponent<Button>();
+
+        // 添加按钮点击事件
+        searchButton.onClick.AddListener(SearchData);
+    }
+
+    void SearchData() => StartCoroutine(SearchData_Coroutine());
+
+    IEnumerator SearchData_Coroutine()
+    {
+        // 检查是否输入了关键词
+        if (string.IsNullOrEmpty(inputKeyword.text))
+        {
+            outputArea.text = "请输入关键词！";
+            yield break;
+        }
+
+        outputArea.text = "Loading...";
+        string keyword = inputKeyword.text;
+
+        // 对关键词进行URL编码
+        string encodedKeyword = UnityWebRequest.EscapeURL(keyword);
+
+        // 设置请求URL
+        string url = "http://127.0.0.1:8000/search/?input_keyword=" + encodedKeyword;
+
+        // 创建GET请求
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            // 发送请求并等待响应
+            yield return request.SendWebRequest();
+
+            // 检查请求结果
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                outputArea.text = "错误：" + request.error;
+            }
+            else
+            {
+                outputArea.text = request.downloadHandler.text;
+            }
+        }
+    }
+}
